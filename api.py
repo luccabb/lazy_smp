3c497446e2ad6062a852e1ff31d4a24ca3cdd474
luccabb <luccabertoncini@minerva.kgi.edu>
2022-01-26 15:59:15 -0800

    api cleaning

--- api.py
+++ api.py
@@ -1,17 +1,12 @@
+import chess
+import chess.polyglot
 from flask import request
 from flask_cors import CORS, cross_origin
 from flask import Flask
-import chess
-import chess.polyglot
-import time
-import helper
+from helper import get_engine
 from typing import Dict, Any
-import multiprocessing as mp
+from multiprocessing import set_start_method
 
-
-# Search constants
-R = 2
-COUNTER = 0
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -33,7 +28,6 @@ def format_response(best_move: str) -> Dict[str, Any]:
 @app.route('/')
 @cross_origin()
 def main_search() -> Dict[str, Any]:
-	st = time.time()
 	fen = request.args.get('fen')
 	board = chess.Board(fen)
 
@@ -41,25 +35,20 @@ def main_search() -> Dict[str, Any]:
 	# ALGORITHM_NAME = "parallel_alpha_beta_layer_1"
 	ALGORITHM_NAME = "parallel_alpha_beta_layer_2"
 	# ALGORITHM_NAME = "lazy_smp"
+	DEPTH = 3
+	NULL_MOVE = True
 
-	engine = helper.get_implementation(ALGORITHM_NAME)
-
-	depth = 3
-	player = 1
-	null_move = True
+	engine = get_engine(ALGORITHM_NAME)
 	
 	# using cerebellum opening book: https://zipproth.de/Brainfish/download/
 	try:
 		best_move = chess.polyglot.MemoryMappedReader("opening_book/cerebellum.bin").weighted_choice(board).move().uci()
-		return format_response(best_move)
 	except:
-		best_move = engine(board, depth, player, null_move)
+		best_move = engine(board, DEPTH, NULL_MOVE)
 	
-	end = time.time()
-	print((end - st) * 1000)
 	return format_response(best_move)
 
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000, debug=True)
-	mp.set_start_method("chess")
+	set_start_method("chess")
