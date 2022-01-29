adc0ed1310fbbb69a1467787b9c3c87356da6dcd
luccabb <luccabertoncini@minerva.kgi.edu>
2022-01-28 18:01:01 -0800

    lazy smp simplification

--- lazy_smp.py
+++ lazy_smp.py
@@ -1,11 +1,6 @@
-from chess import Board, Move
-from typing import Tuple, Union
-from multiprocessing import cpu_count, Manager, Process
-from move_ordering import organize_moves
-from quiescence import quiescence_search
+from chess import Board
+from multiprocessing import cpu_count, Manager, Process, Pool
 from base_engine import ChessEngine
-from psqt import board_evaluation
-from constants import CHECKMATE_SCORE, CHECKMATE_THRESHOLD, NULL_MOVE_R, QUIESCENCE_SEARCH_DEPTH
 
 
 class LazySMP(ChessEngine):
@@ -15,27 +10,14 @@ class LazySMP(ChessEngine):
 
         # getting number of processors
         nprocs = cpu_count()
+        pool = Pool(processes=nprocs)
 
-        # start manager so that we can share data between processes
-        with Manager() as manager:
-            
-            # create shared hash table
-            shared_hash_table = manager.dict()
-            processes = []
-            
-            # start search for all processors from the root node
-            for _ in range(nprocs):
-                p = Process(target=self.negamax, args=(board, depth, null_move, shared_hash_table))
-                p.start()
-                processes.append(p)
-            
-            # wait for all processes to finish
-            for process in processes:
-                process.join()
+        manager = Manager()
+        # create shared hash table
+        shared_hash_table = manager.dict()
+        # executing all the moves at layer 1 in parallel
+        # starmap blocks until all process are done
+        pool.starmap(self.negamax, args=(board, depth, null_move, shared_hash_table))
 
-            # close all ongoing processes
-            for p in processes:
-                p.terminate()
-            
-            # return best move for our original board
-            return shared_hash_table[(board.fen(), depth)][1]
+        # return best move for our original board
+        return shared_hash_table[(board.fen(), depth)][1]
