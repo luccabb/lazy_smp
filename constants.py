09995c083d314130cc1881b24ef27e847e3e4488
luccabb <luccabertoncini@minerva.kgi.edu>
2022-02-27 02:47:26 -0800

    negamax naming

--- constants.py
+++ constants.py
@@ -6,13 +6,13 @@ Definition of search constants
         - parallel_alpha_beta_layer_1: parallel alpha beta search from 1st ply.
         - parallel_alpha_beta_layer_2: parallel alpha beta search from 2nd ply.
         - lazy_smp: lazy smp search.
-    DEPTH: Depth of search.
+    NEGAMAX_DEPTH: Depth of negamax search.
     NULL_MOVE: If True, use null move prunning.
     NULL_MOVE_R: Null move reduction factor.
     CHECKMATE_SCORE: Score for checkmate.
     CHECKMATE_THRESHOLD: Threshold to differentiate 
         checkmates from other moves.
-    QUIESCENCE_SEARCH_DEPTH: Depth to search in quiescence search.
+    QUIESCENCE_SEARCH_DEPTH: Max depth of quiescence search.
 
 """
 ALGORITHM_NAME = "parallel_alpha_beta_layer_2"
